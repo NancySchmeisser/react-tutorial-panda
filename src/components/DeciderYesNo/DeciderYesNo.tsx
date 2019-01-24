2c783a37f8a88287927bbf48011d131bf9ed998b
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-24 11:17:17 +0100

    feat: implemented DeciderYesNo

--- src/components/DeciderYesNo/DeciderYesNo.tsx
+++ src/components/DeciderYesNo/DeciderYesNo.tsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
+class DeciderYesNo extends Component<any, any> {
 
-class DeciderYesNo extends Component {
-  render() {
-    return (
-      <div className="DeciderYesNo">
-        <span>Test</span>
-      </div>
-    );
-  }
+    private yesTexts: string[] = ["Ja", "Jawoll", "Sicher", "Klar"];
+    private noTexts: string[] = ["Nein", "Niemals", "Neehe"];
+    
+    constructor (props:any) {
+        super(props);
+
+        const yesNo: number=Math.round(Math.random());
+        const textlist=yesNo==0 ? this.yesTexts : this.noTexts;
+        const textIndex=Math.round(Math.random() * (textlist.length -1) )
+        this.state={decision: textlist[textIndex]}
+    }
+    render() {
+        return (
+            <div className="DeciderYesNo">
+                <span>{this.state.decision}</span>
+            </div>
+        );
+    }
 }
 
 export default DeciderYesNo;
