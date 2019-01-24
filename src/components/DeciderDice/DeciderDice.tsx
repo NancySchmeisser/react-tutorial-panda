d713ced2326a213fb1eadd7fcf0172dae632c659
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-24 12:08:05 +0100

    feat: added images, gif, implemented DeciderDice

--- src/components/DeciderDice/DeciderDice.tsx
+++ src/components/DeciderDice/DeciderDice.tsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import One from './One.png';
+import Two from './Two.png';
+import Three from './Three.png';
+import Four from './Four.png';
+import Five from './Five.png';
+import Six from './Six.png';
+import Animated from './animated.gif';
+import './DeciderDice.css';
+
 
 class DeciderDice extends Component<any, any> {
 
+    private diceImages: string[]=[One, Two, Three, Four, Five, Six]
    
     constructor (props:any) {
         super(props);
 
-      
+        const result=Math.round(Math.random() * 5 )+1
+        this.state={result:result, image:Animated}
+        setTimeout(()=>{
+            this.setState({image:this.diceImages[this.state.result -1]})
+        },1000)
     }
     render() {
         return (
             <div className="DeciderDice">
-               
+               <img className="DeciderDice" src={this.state.image}/>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DeciderYesNo from './components/DeciderYesNo/DeciderYesNo';
 import * as serviceWorker from './serviceWorker';
+import DeciderYesNo from './components/DeciderYesNo/DeciderYesNo';
+import DeciderDice from './components/DeciderDice/DeciderDice';
 
-ReactDOM.render(<DeciderYesNo />, document.getElementById('root'));
+ReactDOM.render(<div><DeciderYesNo /><DeciderDice /><DeciderDice /></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
